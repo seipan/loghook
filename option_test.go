894e5b7c5b8b1d1f091881ccffa5391252d60ce4
seipan <yamasakipann0218@gmail.com>
2023-12-16 19:51:51 +0900

    test(option) : Add OptionTest

--- option_test.go
+++ option_test.go
@@ -1,1 +1,16 @@
 package loghook
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestOption(t *testing.T) {
+	t.Run("NewOption", func(t *testing.T) {
+		t.Run("success", func(t *testing.T) {
+			option := NewOption("discord")
+			assert.Equal(t, "discord", option.Types())
+		})
+	})
+}
