f68ccb80c048bab0fb1f9eff241252ddca4afd3a
seipan <yamasakipann0218@gmail.com>
2024-02-23 02:46:27 +0900

    feat : Add slack send e2e test

--- test/test.go
+++ test/test.go
@@ -9,6 +9,7 @@ import (
 func eToeTest(discordWebhookURL string, slackWebhookURL string) error {
 	testNosend()
 	testDiscordSend(discordWebhookURL)
+	testSendSlack(slackWebhookURL)
 	return nil
 }
 
@@ -28,6 +29,28 @@ func testDiscordSend(discordWebhookURL string) {
 	logger := loghook.NewLogger("", "test", "discord", discordWebhookURL)
 	logger.SetLevel(loghook.DebugLevel)
 	logger.SetWebhook(discordWebhookURL)
+
+	logger.Debug("test")
+	logger.DebugContext(context.Background(), "test")
+	logger.Debugf("test %s", "debug")
+	logger.Info("test")
+	logger.InfoContext(context.Background(), "test")
+	logger.Infof("test %s", "info")
+	logger.Warn("test")
+	logger.WarnContext(context.Background(), "test")
+	logger.Warnf("test %s", "warn")
+	logger.Error("test")
+	logger.ErrorContext(context.Background(), "test")
+	logger.Errorf("test %s", "error")
+	logger.Fatal("test")
+	logger.FatalContext(context.Background(), "test")
+	logger.Fatalf("test %s", "fatal")
+}
+
+func testSendSlack(slackWebhookURL string) {
+	logger := loghook.NewLogger("", "test", "slack", slackWebhookURL)
+	logger.SetLevel(loghook.DebugLevel)
+	logger.SetWebhook("")
 
 	logger.Debug("test")
 	logger.DebugContext(context.Background(), "test")
